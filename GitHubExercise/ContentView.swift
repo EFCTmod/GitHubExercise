d5bcf8b646a1b5921213f94bf071555318f98852
Rick Reyna <Ricksabp@gmail.com>
2024-05-20 16:00:22 -0500

    1 Circle Version 1.0.1

--- GitHubExercise/ContentView.swift
+++ GitHubExercise/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.red)
+            Text("Version 1.0.1")
         }
         .padding()
     }
